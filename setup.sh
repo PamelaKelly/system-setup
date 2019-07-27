a7a11b8de14cb971900dae6d80fabb0d7009c6f1
PamelaKelly <pamelakelly7@gmail.com>
2019-07-27 21:01:06 +0100

    Initial setup script with Golang, K8s, Curl and system update

---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Update System
+sudo apt-get update
+sudo apt-get -y upgradei
+
+# Basics
+sudo apt install curl -y
+ 
+# Install latest stable version of Golang
+sudo snap install go --classic
+
+# Install latest version of kubectl
+sudo snap install kubectl --classic
+
+# Install Git
+sudo apt install git -y
+
+# Set up ssh key
+# mkdir ~/.ssh/
+# ssh-keygen -t rsa -b 4096 -C "your email"
+# Enter target directory and passphrase when prompted
+# Start the agent in the background
+# eval "$(ssh-agent -s)"
+# Add the ssh key 
+# ssh-add ~/.ssh/id_rsa
+
+
